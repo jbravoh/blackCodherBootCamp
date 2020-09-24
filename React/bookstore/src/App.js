3ba19a8cf4028db516d2f7312d6d387258714dda
jbravoh <68926455+jbravoh@users.noreply.github.com>
2020-09-24 20:48:20 +0100

    created bookstore

--- React/bookstore/src/App.js
+++ React/bookstore/src/App.js
@@ -3,19 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const h1Style = { "fontSize":"60px", "color":"yellow"};
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 style={h1Style}className="h1Style">My First React App</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Learn React
+        </a>
       </header>
     </div>
   );
-  
-
-  
 }
 
 export default App;
--- React/myfirstapp/src/App.test.js
+++ React/myfirstapp/src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from './App'; //importing the app from App.js
 
-test('renders learn react link', () => {
+test('renders learn react link', () => { // renders app from App.js - this npm test passed
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElement = getByText(/learn react/i); // when we're searching the 'getByText' component it is searching for (/learn react/)
+  expect(linkElement).toBeInTheDocument(); // either true or false (true = passed, false = fail)
 });
+
+it('renders without crashing', () => { // this npm test failed
+  const div = document.createElement('div');
+  ReactDOM.render(<App />,div);
+  });
