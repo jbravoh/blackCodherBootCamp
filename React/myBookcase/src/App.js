b39a175b2fa3feb90b7b135ef3c4c0efe237957a
jbravoh <68926455+jbravoh@users.noreply.github.com>
2020-10-17 14:05:03 +0100

    fixed the booklist remove issue

--- React/myBookcase/src/App.js
+++ React/myBookcase/src/App.js
@@ -1,91 +1,97 @@
-import React, {useState} from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Header from './components/Header';
-import BookList from './components/BookList';
-import data from './models/books.json';
-import About from './pages/About';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import BookList from "./components/BookList";
+import data from "./models/books.json";
+import About from "./pages/About";
 // import Search from "./components/Search";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './stylesheets/Home.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./stylesheets/Home.css";
 
-
-const App = (props) => { 
-  const [books, setBooks] = useState(data); 
-  const [keyword, setKeyword] = useState('');
+const App = (props) => {
+  const [books, setBooks] = useState(data);
+  const [keyword, setKeyword] = useState("");
   const [bookcase, setBookcase] = useState([]); // this is to add books to the bookcase
-  
 
-  function addBook(title, id) { // this function remove the book when it is clicked 
-    const newBookList = books.filter(book => book.id !== id); // this only filt
-    const chosenBook = books.filter(book => book.id === id);
+  function addBook(title, id) {
+    // this function remove the book when it is clicked
+    const newBookList = books.filter((book) => book.id !== id); // this only filt
+    const chosenBook = books.filter((book) => book.id === id);
     setBooks(newBookList);
-    setBookcase([...bookcase, ...chosenBook]); // create an array and keep storing 
+    setBookcase([...bookcase, ...chosenBook]); // create an array and keep storing
 
-  //   const remainingBooks = []
-  //   let chosenBook = null
-  //   books.forEach((book) => {
-  //   if (book.id !== id) {
-  //     remainingBooks.push(book);
-  //   } else {
-  //     chosenBook = book;
-  //   }
-  // // });
+    //   const remainingBooks = []
+    //   let chosenBook = null
+    //   books.forEach((book) => {
+    //   if (book.id !== id) {
+    //     remainingBooks.push(book);
+    //   } else {
+    //     chosenBook = book;
+    //   }
+    // });
 
-  // setBooks(remainingBooks);
-  // setBookcase([...bookcase, chosen]);
+    // setBooks(remainingBooks);
+    // setBookcase([...bookcase, chosenBook]);
 
-    console.log(`The book ${title} was clicked`)
-  } 
+    console.log(`The book ${title} was clicked`);
+  }
 
   function removeBook(id) {
-    const newBookcaseList = books.filter(book => book.id !== id); 
+    const newBookcaseList = bookcase.filter((book) => book.id !== id);
+
     setBookcase(newBookcaseList);
   }
 
-  async function findBooks(term) { 
-    const results = await fetch
-    (`https://www.googleapis.com/books/v1/volumes?q=${term}&filter=paid-ebooks
-&print-type=books&projection=lite`).then(res => res.json());
+  async function findBooks(term) {
+    const results = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${term}&filter=paid-ebooks
+&print-type=books&projection=lite`).then((res) => res.json());
     setBooks(results.items);
-  
   }
 
   // This return renders the navigation menu from Header.js
   return (
     <>
       <Router>
-        <Route exact path="/" render={() => (
-          <React.Fragment>
-            <Header findBooks={findBooks} keyword={keyword} setKeyword={setKeyword} /> 
-            {/* <Search findBooks={findBooks} keyword={keyword} setKeyword={setKeyword} /> */}
-            <BookList books={books} addBook={addBook} />
-          </React.Fragment>
-        )} />
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <React.Fragment>
+              <Header
+                findBooks={findBooks}
+                keyword={keyword}
+                setKeyword={setKeyword}
+              />
+              {/* <Search findBooks={findBooks} keyword={keyword} setKeyword={setKeyword} /> */}
+              <BookList books={books} addBook={addBook} />
+            </React.Fragment>
+          )}
+        />
 
-        <Route exact path="/about" render={() => (
-          <React.Fragment>
-            <Header />
-            <About />  
-          </React.Fragment>
-        )} /> 
-  
+        <Route
+          exact
+          path="/about"
+          render={() => (
+            <React.Fragment>
+              <Header />
+              <About />
+            </React.Fragment>
+          )}
+        />
 
-        <Route exact path="/bookcase" render={() => (
-          <React.Fragment>
-            <Header />
-            <BookList books={bookcase} removeBook={removeBook}/>
-          </React.Fragment>
-        )} />
+        <Route
+          exact
+          path="/bookcase"
+          render={() => (
+            <React.Fragment>
+              <Header />
+              <BookList books={bookcase} removeBook={removeBook} />
+            </React.Fragment>
+          )}
+        />
       </Router>
     </>
   );
-}
-
-
+};
 
 export default App;
-
-
-
-
-
--- React/myBookcase/src/components/Header.js
+++ React/myBookcase/src/components/Header.js
@@ -1,27 +1,34 @@
-import React, {useState} from "react";
-import {Link} from 'react-router-dom';
-import '../stylesheets/Header.css';
-import Search from './Search';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../stylesheets/Header.css";
+import Search from "./Search";
 
 const Header = (props) => {
-    // const [keyword, setKeyword] = useState('');
+  // const [keyword, setKeyword] = useState('');
 
-    return (
-        <React.Fragment>
-        <div className="headerContainer">
-            <h1 className="appTitle">My Bookcase</h1>
-            <div className="linkContainer">
-                <Link className="navLink" to ="/">Home</Link>
-                <Link  className="navLink" to ="/about">About</Link>
-                <Link className="navLink" to ="/bookcase">Bookcase</Link>
-            </div>
-            <Search findBooks={props.findBooks} keyword={props.keyword} setKeyword={props.setKeyword} />
+  return (
+    <React.Fragment>
+      <div className="headerContainer">
+        <h1 className="appTitle">My Bookcase</h1>
+        <div className="linkContainer">
+          <Link className="navLink" to="/">
+            Booklist
+          </Link>
+          <Link className="navLink" to="/about">
+            About
+          </Link>
+          <Link className="navLink" to="/bookcase">
+            Bookcase
+          </Link>
         </div>
-        </React.Fragment>
-    )
-}
-
-
-
+        <Search
+          findBooks={props.findBooks}
+          keyword={props.keyword}
+          setKeyword={props.setKeyword}
+        />
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Header; // always need to export page
